b6095321ed5b2e5d391cfa1ec14fb0308636bfd6
Murtaja Khammash <103779289+murtajak@users.noreply.github.com>
2026-03-21 00:44:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using LibrarySystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibrarySystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<LibrarySystemContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryConn") ?? throw new InvalidOperationException("Connection string 'LibrarySystemContext' not found.")));
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("SecurityConn") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<SecuirtyDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<SecuirtyDbContext>();
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Books}/{action=Index}/{id?}")
+                .WithStaticAssets();
+            app.MapRazorPages()
+               .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
